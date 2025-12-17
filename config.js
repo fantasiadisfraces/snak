63e010fe530f8030bef10335b39d55f79aec7445
fantasiadisfraces <fantasiadisfraces1@gmail.com>
2025-12-17 10:01:47 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+// ========================================
+// ARCHIVO DE CONFIGURACIÓN - Sistema POS v2.1
+// ========================================
+
+const CONFIG = {
+    // ID de tu Google Sheet
+    GOOGLE_SHEET_ID: '1AmFocVwvywXz6LOwggkFscXjEhx_FZvZCVmb-1ihm5I',
+    
+    // Credenciales de Google API
+    CLIENT_ID: '488089624210-ns62tr4g9rqov3k2b85965c4p4fto028.apps.googleusercontent.com',
+    API_KEY: 'AIzaSyDsIk-N9hDAzZN7vc9b2rUIhcA7D8ViOFk',
+    
+    // NOMBRES DE LAS 5 HOJAS - Deben coincidir EXACTAMENTE
+    SHEETS: {
+        CATEGORIAS: 'Categorias',
+        PRODUCTOS: 'Productos', 
+        ACOMPAÑAMIENTOS: 'Acompañamientos',
+        VENTAS: 'Ventas',
+        DETALLE_VENTAS: 'Detalle_Ventas'
+    }
+};
