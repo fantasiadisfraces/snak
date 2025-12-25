730abe4a9430667884c2179fc0b9b0370f843083
fantasiadisfraces <fantasiadisfraces1@gmail.com>
2025-12-25 15:47:41 -0400

    Update config.js

--- config.js
+++ config.js
@@ -26,8 +26,8 @@ const CONFIG = {
     // CLIENT_ID: Identifica la aplicación ante Google
     // API_KEY: Clave para acceder a la API de Google Sheets
     // ══════════════════════════════════════════════════════════════════════════
-    CLIENT_ID: '814005655098-8csk41qts3okv4b2fjnq7ls4qc2kq0vc.apps.googleusercontent.com',
-    API_KEY: 'AIzaSyAOhGTjJXHhuUhqf1g2DPCla59xNzftb-Q',
+    CLIENT_ID: '488089624210-ns62tr4g9rqov3k2b85965c4p4fto028.apps.googleusercontent.com',
+    API_KEY: 'AIzaSyDsIk-N9hDAzZN7vc9b2rUIhcA7D8ViOFk',
     
     // ══════════════════════════════════════════════════════════════════════════
     // NOMBRES DE LAS 5 HOJAS DE LA BASE DE DATOS
